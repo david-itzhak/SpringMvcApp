72146ddb1bc66d765b24397b30924887906e9115
David Itzhak <daviditzhak7@gmail.com>
2021-01-18 15:55:53 +0200

    Used parameters in Get query. Used HttpServletRequest object or @RequestParam annotation. (see file FirstController.java).

--- src/main/java/izhak/springmvc/controllers/FirstController.java
+++ src/main/java/izhak/springmvc/controllers/FirstController.java
@@ -1,8 +1,12 @@
 package izhak.springmvc.controllers;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author David Izhak
@@ -11,7 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(){
+    /*public String helloPage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");*/
+    /*public String helloPage(@RequestParam("name"e) String name,
+                            @RequestParam("surname"e) String surname){*/
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
 
