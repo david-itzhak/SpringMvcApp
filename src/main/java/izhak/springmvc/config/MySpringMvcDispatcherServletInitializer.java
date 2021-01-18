b921d60ea2cd61688b5d10c8402857ce6ef6fb07
David Itzhak <daviditzhak7@gmail.com>
2021-01-18 12:40:30 +0200

    Replaced XML spring MVS config on java-class config Added two classes: - MySpringMvcDispatcherServletInitializer.java, - SpringConfig.java. Deleted two files: - web.xml, - applicationContextMVC.xml. Added to pom.xml one dependency on javax.servlet-api.

---
+++ src/main/java/izhak/springmvc/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package izhak.springmvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author David Izhak
+ */
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
